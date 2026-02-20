d80f2cad7dec5dd50c4fdcb2b2fe226805f9a5cd
KSOIS <kitylive112@gmail.com>
2026-02-20 01:34:41 +0100

    Ejecutador de programas agregado

---
+++ ejecutador de archivos.py
@@ -0,0 +1,27 @@
+import os
+print("hello world")
+path = os.getcwd()
+archivosArray = []
+rutaArchivos = []
+print(path)
+def verificarArchivos(path):
+    for archivos in os.listdir(path):
+        pathActual = os.path.join(path, archivos)
+        if os.path.isdir(pathActual):
+            verificarArchivos(pathActual)
+        elif os.path.isfile(pathActual):
+            extension = pathActual.split('.')[-1]
+            if extension == "py":
+                archivosArray.append(archivos)
+                rutaArchivos.append(pathActual)
+def ejecutarArchivos(indice):
+     os.startfile(rutaArchivos[indice])
+def printearArchivos():
+    for i in range(len(archivosArray)):
+        print(f"{i+1}.{archivosArray[i]}")
+def pedirOpcion():
+    opcion = int(input("¿Que programa quieres ejecutar?: "))
+    ejecutarArchivos(opcion-1)
+verificarArchivos(path)
+printearArchivos()
+pedirOpcion()
